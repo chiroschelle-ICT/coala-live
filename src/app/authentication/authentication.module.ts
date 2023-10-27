73ad7f66dfc4d907f960e7e54b202ed9ade707e7
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2023-10-27 12:43:51 +0200

    Unit test succes signup

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,18 +1,18 @@
+// base
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// Modules
 import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+// Firebase
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../../../environments';
-
-import { provideAuth, getAuth } from '@angular/fire/auth';
+// Component
 import { SignupComponent } from './signup/signup.component';
 import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
