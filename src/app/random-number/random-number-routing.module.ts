636d735dfd96453c5685294918da16c3722e38b0
Killian-Stella <killian.serluppens@gmail.com>
2023-11-04 01:51:31 +0100

    guards

--- src/app/random-number/random-number-routing.module.ts
+++ src/app/random-number/random-number-routing.module.ts
@@ -4,9 +4,10 @@ import { Child1Component } from './child1/child1.component';
 import { Child2Component } from './child2/child2.component';
 import { RouterModule, Routes } from '@angular/router'; // Import RouterModule and Routes from '@angular/router'
 import { ParentComponent } from './parent/parent.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'subParent', component: ParentComponent },
+  { path: 'subParent', component: ParentComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
