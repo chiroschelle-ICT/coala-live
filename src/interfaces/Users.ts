0e14b64733131528cdef3f255f204f4852ba48d6
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2023-12-22 01:22:38 +0100

    FIxed Pull issues

--- src/interfaces/Users.ts
+++ src/interfaces/Users.ts
@@ -5,17 +5,17 @@ export class Users
     afdeling: string;
     afdelingId: string;
     email: string;
-    wachtwoord: string;
+    password: string;
     rights: number;
     userId: string;
-    constructor(Id:null, name:string, email:string, afdeling:string, afdelingId:string, wachtwoord:string, rights:number, userId:string)
+    constructor(Id:null, name:string, email:string, afdeling:string, afdelingId:string, password:string, rights:number, userId:string)
     {
         this.Id = Id;
         this.name = name;
         this.email = email;
         this.afdeling = afdeling;
         this.afdelingId = afdelingId;
-        this.wachtwoord = wachtwoord;
+        this.password = password;
         this.rights = rights;
         this.userId = userId;
     }
