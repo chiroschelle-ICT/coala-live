ba1fbc8cb51d452e583815404c2b21a965e96d48
Killian-Stella <killian.serluppens@gmail.com>
2024-03-14 23:36:09 +0100

    Edit Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,9 +9,9 @@ import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent,  canActivate: [AuthGuard]  },
-  { path: 'afdelingen', component: AfdelingenComponent, canActivate: [AuthGuard ] },
-  { path: 'afdelingLijst/:afdelingParId', component: AfdelingLijstComponent, canActivate: [AuthGuard] },
-  { path: 'ledenDetails/:lidId', component: LedenDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'afdelingen', component: AfdelingenComponent, /* canActivate: [AuthGuard ] */ },
+  { path: 'afdelingLijst/:afdelingParId', component: AfdelingLijstComponent, /* canActivate: [AuthGuard] */ },
+  { path: 'ledenDetails/:lidId', component: LedenDetailsComponent, /* canActivate: [AuthGuard] */ },
   { path: 'addLid', component: AddLidComponent, canActivate: [AuthGuard] },
   { 
     path: 'authentication',
