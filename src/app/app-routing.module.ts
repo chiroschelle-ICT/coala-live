a1593768ec7e642c1549db811b9b4c90a807a4c6
Killian-Stella <killian.serluppens@gmail.com>
2024-08-30 18:03:44 +0200

    rand

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ import { AddLidComponent } from './add-lid/add-lid.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent,  canActivate: [AuthGuard]  },
+  { path: '', component: DashboardComponent,  /* canActivate: [AuthGuard] */  },
   { path: 'afdelingen', component: AfdelingenComponent, /* canActivate: [AuthGuard ] */ },
-  { path: 'afdelingLijst/:afdelingParId', component: AfdelingLijstComponent, /* canActivate: [AuthGuard] */ },
-  { path: 'ledenDetails/:lidId', component: LedenDetailsComponent, /* canActivate: [AuthGuard] */ },
-  { path: 'addLid', component: AddLidComponent, canActivate: [AuthGuard] },
+  { path: 'afdelingLijst/:afdelingParId', component: AfdelingLijstComponent,/* canActivate: [AuthGuard] */},
+  { path: 'ledenDetails/:lidId', component: LedenDetailsComponent, /* canActivate: [AuthGuard] */},
+  { path: 'addLid', component: AddLidComponent, /* canActivate: [AuthGuard] */ },
   { 
     path: 'authentication',
     loadChildren: () => import('./authentication/authentication-routing.module').then(m => m.AuthenticationRoutingModule)
